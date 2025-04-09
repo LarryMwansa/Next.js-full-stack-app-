d1fec4fdb98815207a2f7fbcf58ca06c8d10ad56
Larry Mwansa <mwansaplarry@gmail.com>
2025-04-09 07:40:52 +0200

    Added: - Home page hero - About page and css - footer component

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import styles from './admin.module.css'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div><h1>DashBoard</h1></div>
   )
 }
 
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+import Image from "next/image";
 
 const links = [
   {
@@ -49,13 +50,14 @@ const Links = () => {
           <NavLink item={{ title: "Login", path: "/login" }} />
         )}
       </div>
-      <button
+      <Image
         className={styles.menuButton}
-        type="button"
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
         onClick={() => setOpen((prev) => !prev)}
-      >
-        Menu
-      </button>
+      />
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
